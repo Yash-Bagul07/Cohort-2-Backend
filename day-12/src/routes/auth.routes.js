18179b1372c9f5d9f90e5934f438bab818334286
Yash Bagul <yashbagul100@gmail.com>
2026-02-27 03:32:31 +0530

    TIll cookie

---
+++ day-12/src/routes/auth.routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const authRouter= express.Router();
+
+authRouter.post("/register" ,async (req,res)=>{
+    const {name, email, password} = req.body
+
+    const isUserAlreadyExist = await userModel.findOne({email})
+
+    if(isUserAlreadyExist){
+        return res.status(400).json({
+            message:"User already exists with this email"
+        })
+    }
+
+   const user = await userModel.create({
+        name,email,password
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        email: user.email
+    },
+    process.env.JWT_SECERT,)
+
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message:"User registered successfully",
+        user,
+        token
+    })
+})
+
+module.exports = authRouter;
